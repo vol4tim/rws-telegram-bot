6a508a54d1117a00c8249ece1fbb9a32b9c3f8bd
vol4tim <vol4tim@gmail.com>
2022-06-29 14:30:25 +0400

    add address to logger

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ async function runApp() {
           `Your current address ${address} was successfully added to subscription! Please <a href='https://dapp.robonomics.network/#/lights-up'>go to Lights up (d)app</a>`
         );
       } catch (error) {
-        logger.error("save", error);
+        logger.error(`save | ${user.address} | ${address}`, error);
         return ctx.reply(error.message);
       }
     } else {
